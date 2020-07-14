f08d67f3e5f880fb172105c20dcdb408ba3358ab
Kevin Kreitner <kevin.kreitner@real-digital.de>
2020-07-14 21:53:55 +0200

    Initial commit

---
+++ src/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var lastHash = ""
+
+func main() {
+	app := &cli.App{
+		Name:  "pcheck",
+		Usage: "Page Checker to observe changes on web pages.",
+		Commands: cli.Commands{
+			{
+				Name: "observe",
+				Action: func(c *cli.Context) error {
+					uri := c.String("uri")
+					webHookUri := c.String("webHookuri")
+					listenForChangesOn(uri, webHookUri)
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "uri",
+						Aliases:  []string{"u"},
+						Usage:    "Uri of the content you want to observe",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "webHookuri",
+						Aliases:  []string{"wu"},
+						Usage:    "Weebhook Url to push notification to.",
+						Required: true,
+					},
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func listenForChangesOn(url string, weebhookUri string) {
+	done := make(chan bool)
+
+	log.Printf("Start observing page: %s.", url)
+
+	go func() {
+		for {
+			if !checkPageForChanges(url) {
+				time.Sleep(10 * time.Second)
+				continue
+			}
+			_ = SendSlackNotification(
+				weebhookUri,
+				fmt.Sprintf("Page under %s has changed!", url),
+				"")
+			done <- true
+		}
+	}()
+
+	<-done
+	log.Printf("Listening for changes on %s stopped.", url)
+}
+
+func checkPageForChanges(url string) bool {
+	h := md5.New()
+	log.Println("Request sent")
+	response, err := http.Get(url)
+	if err != nil {
+		log.Printf("No response from url with error: %s", err)
+		return false
+	}
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	h.Write(bodyBytes)
+	md5Hash := hex.EncodeToString(h.Sum(nil))
+	if lastHash == "" {
+		lastHash = md5Hash
+		return false
+	}
+	if md5Hash == lastHash {
+		log.Println("No content changes yet.")
+		return false
+	}
+	log.Printf("Last hash: %s", lastHash)
+	log.Printf("Side hash: %s", md5Hash)
+	log.Println("Send slack notification and finish.")
+	return true
+}
---
+++ src/slack.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+)
+
+const SuccessColor = "#2EB886"
+
+type SlackRequestBody struct {
+	Pretext string `json:"pretext"`
+	Text    string `json:"text"`
+	Color   string `json:"color"`
+}
+
+// SendSlackNotification will post to an 'Incoming Webhook' url setup in Slack Apps. It accepts
+// some text and the slack channel is saved within Slack.
+func SendSlackNotification(webhookUrl string, pretext string, text string) error {
+	slackBody, _ := json.Marshal(SlackRequestBody{Pretext: pretext, Text: text, Color: SuccessColor})
+	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if buf.String() != "ok" {
+		return errors.New("Non-ok response returned from Slack")
+	}
+	return nil
+}
